967b5d03609ee60343dc99a4fa9168c328a6027b
LocTM <tranminhloc.anhk@gmail.com>
2023-11-19 20:56:39 +0700

    BT1

---
+++ index.js
@@ -0,0 +1,13 @@
+// BT1
+// let daySoChan = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// let soLuong = demSoChan(daySoChan);
+// function demSoChan(daySoChan) {
+//     let soLuong = 0;
+//     for (let i = 0; i < daySoChan.length; i++) {
+//         if (daySoChan[i] % 2 === 0) {
+//             soLuong++;
+//         }
+//     }
+//     return soLuong;
+// }
+// console.log("Tổng số lượng số chẵn là: " + soLuong);
