79a7b9c37dc35e475b1528cb5b1b729ff7e47738
LocTM <tranminhloc.anhk@gmail.com>
2023-11-19 21:06:38 +0700

    BT2

--- index.js
+++ index.js
@@ -11,3 +11,22 @@
 //     return soLuong;
 // }
 // console.log("Tổng số lượng số chẵn là: " + soLuong);
+
+//BT2
+// function demSoLuongPhanTu(mang, giaTriCanDem) {
+//     let soLuong = 0;
+//     for (let i = 0; i < mang.length; i++) {
+//         if (mang[i] === giaTriCanDem) {
+//             soLuong++;
+//         }
+//     }
+//     return soLuong;
+// }
+// let mang = [1, 2, 3, 4, 2, 5, 2, 6, 7, 8, 9, 12, -1, 4, 8, 1, 10];
+// let giaTriCanDem = +prompt("Nhập giá trị a: ");
+// let soLuong = demSoLuongPhanTu(mang, giaTriCanDem);
+// console.log(`Số lượng phần tử có giá trị bằng ${giaTriCanDem} trong mảng là: ${soLuong}`);
+
+
+
+
